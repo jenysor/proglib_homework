25cb4da9b4a2bf9d05af8d0a58c0635f2fce9732
jenysor <zheny.sor@yandex.ru>
2025-07-11 19:22:41 +0300

    Revert "экспорт символов в либах"
    This reverts commit 93b6fbb105772305d747159b273121ea1fbd01a9.

--- movable/include/movable.h
+++ movable/include/movable.h
@@ -1,14 +1,8 @@
 #ifndef PROGLIB_HOMEWORK_MOVABLE_H
 #define PROGLIB_HOMEWORK_MOVABLE_H
 
-#ifdef MOVABLE_EXPORTS
-#define MOVABLE_API __declspec(dllexport)
-#else
-#define MOVABLE_API __declspec(dllimport)
-#endif
+class IMovable {};
 
-class MOVABLE_API IMovable {};
-
-class MOVABLE_API Move : public IMovable{};
+class Move : public IMovable{};
 
 #endif //PROGLIB_HOMEWORK_MOVABLE_H
--- rotating/include/rotating.h
+++ rotating/include/rotating.h
@@ -1,14 +1,8 @@
 #ifndef PROGLIB_HOMEWORK_ROTATING_H
 #define PROGLIB_HOMEWORK_ROTATING_H
 
-#ifdef ROTATING_EXPORTS
-#define ROTATING_API __declspec(dllexport)
-#else
-#define ROTATING_API __declspec(dllimport)
-#endif
+class IRotating {};
 
-class ROTATING_API IRotating {};
-
-class ROTATING_API Rotate : public IRotating{};
+class Rotate : public IRotating{};
 
 #endif //PROGLIB_HOMEWORK_ROTATING_H
