56f5c3a3528243047972be08c34c454f60a1e267
e.sorokina <e.sorokina@stc-spb.ru>
2025-07-21 15:53:30 +0300

    тесты на move

--- movable/test/test_movable.cpp
+++ movable/test/test_movable.cpp
@@ -1,7 +1,61 @@
-#include "movable.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "movable.h"
 
-TEST(TestMove, Move_Test)
+class MockMovableObject : public IMovable
 {
-    ASSERT_NO_THROW(Move move);
+public:
+    MOCK_METHOD(Coordinates, getPosition, (), (override));
+    MOCK_METHOD(Coordinates, getSpeed, (), (override));
+    MOCK_METHOD(void, setPosition, (const Coordinates& coords), (override));
+};
+
+TEST(TestMove, MovableObject_Move_Success)
+{
+    Coordinates startPosition{.x = 12.0, .y = 5.0};
+    Coordinates speed{.x = -7.0, .y = 3.0};
+
+    auto etalonPosition = startPosition + speed;
+
+    auto object = std::make_shared<MockMovableObject>();
+
+    EXPECT_CALL(*object, getPosition()).WillOnce(testing::Invoke([startPosition]() { return startPosition; }));
+    EXPECT_CALL(*object, getSpeed()).WillOnce(testing::Invoke([speed]() { return speed; }));
+    EXPECT_CALL(*object, setPosition(etalonPosition));
+
+    ASSERT_NO_THROW(Move::move(object));
+}
+
+TEST(TestMove, MovableObject_Move_ErrorGetSpeed)
+{
+    Coordinates startPosition{.x = 12.0, .y = 5.0};
+    Coordinates speed{.x = -7.0, .y = 3.0};
+
+    auto object = std::make_shared<MockMovableObject>();
+
+    EXPECT_CALL(*object, getPosition()).WillOnce(testing::Invoke([startPosition]() { return startPosition; }));
+    EXPECT_CALL(*object, getSpeed())
+        .WillOnce(testing::Invoke(
+            [speed]()
+            {
+                throw std::invalid_argument("can't get speed");
+                return speed;
+            }));
+
+    ASSERT_THROW(Move::move(object), std::invalid_argument);
+}
+
+TEST(TestMove, MovableObject_Move_ErrorGetPosition)
+{
+    Coordinates startPosition{.x = 12.0, .y = 5.0};
+
+    auto object = std::make_shared<MockMovableObject>();
+
+    EXPECT_CALL(*object, getPosition()).WillOnce(testing::Invoke([startPosition]()
+    {
+        throw std::invalid_argument("can't get position");
+        return startPosition;
+    }));
+
+    ASSERT_THROW(Move::move(object), std::invalid_argument);
 }
