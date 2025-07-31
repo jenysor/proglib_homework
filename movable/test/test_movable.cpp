166a6a68d0c9dcc40aa5aab4dcefe3a331256c56
e.sorokina <e.sorokina@stc-spb.ru>
2025-07-31 11:14:24 +0300

    добавлен тест на movable с исключением при setPosition

--- movable/test/test_movable.cpp
+++ movable/test/test_movable.cpp
@@ -51,11 +51,30 @@ TEST(TestMove, MovableObject_Move_ErrorGetPosition)
 
     auto object = std::make_shared<MockMovableObject>();
 
-    EXPECT_CALL(*object, getPosition()).WillOnce(testing::Invoke([startPosition]()
-    {
-        throw std::invalid_argument("can't get position");
-        return startPosition;
-    }));
+    EXPECT_CALL(*object, getPosition())
+        .WillOnce(testing::Invoke(
+            [startPosition]()
+            {
+                throw std::invalid_argument("can't get position");
+                return startPosition;
+            }));
 
     ASSERT_THROW(Move::move(object), std::invalid_argument);
+}
+
+TEST(TestMove, MovableObject_Move_ErrorSetPosition)
+{
+    Coordinates startPosition{.x = 12.0, .y = 5.0};
+    Coordinates velocity{.x = -7.0, .y = 3.0};
+
+    auto etalonPosition = startPosition + velocity;
+
+    auto object = std::make_shared<MockMovableObject>();
+
+    EXPECT_CALL(*object, getPosition()).WillOnce(testing::Invoke([startPosition]() { return startPosition; }));
+    EXPECT_CALL(*object, getVelocity()).WillOnce(testing::Invoke([velocity]() { return velocity; }));
+    EXPECT_CALL(*object, setPosition(etalonPosition))
+        .WillOnce(testing::Invoke([]() { throw std::runtime_error("can't set position"); }));
+
+    ASSERT_THROW(Move::move(object), std::runtime_error);
 }
