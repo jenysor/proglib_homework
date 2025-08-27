94d580b30dcc20b62c32c6648e53a41b598c3ac4
e.sorokina <e.sorokina@stc-spb.ru>
2025-08-27 17:03:52 +0300

    реализация хендлеров и команд

--- exceptionhandler/include/exception_handler.h
+++ exceptionhandler/include/exception_handler.h
@@ -6,64 +6,133 @@
 #include <list>
 #include <utility>
 #include <iostream>
+#include <queue>
 
-//временно
+// временно
 class ICommand {
 public:
-    virtual void execute() = 0;
+	virtual void execute() = 0;
 
-    virtual ~ICommand() = default;
+	virtual ~ICommand() = default;
 };
 
 class IExeptionHandler {
 public:
-    virtual void registerHandler(std::shared_ptr<IExeptionHandler> handler) = 0;
+	[[nodiscard]] virtual bool canHandleException(const std::exception& e) const = 0;
 
-    virtual void handleExeption(std::shared_ptr<ICommand> cmd, const std::exception &e) = 0;
+	virtual void handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const = 0;
+
+	virtual ~IExeptionHandler() = default;
 };
 
-class ExeptionHandler : public IExeptionHandler {
+class HandlerPool {
 public:
-    void registerHandler(std::shared_ptr<IExeptionHandler> handler) override;
+	void registerHandler(std::shared_ptr<IExeptionHandler> handler);
 
-    void handleExeption(std::shared_ptr<ICommand> cmd, const std::exception &e) override;
+	void handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const;
+
+	~HandlerPool() = default;
 
 private:
-    std::list<std::shared_ptr<IExeptionHandler>> handlers;
+	std::list<std::shared_ptr<IExeptionHandler>> handlers;
+};
+
+class WriteExceptionToLogByQueueHandler : public IExeptionHandler {
+public:
+	explicit WriteExceptionToLogByQueueHandler(std::shared_ptr<std::queue<std::shared_ptr<ICommand>>> q);
+
+	[[nodiscard]] bool canHandleException(const std::exception& e) const override;
+
+	void handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const override;
+
+	~WriteExceptionToLogByQueueHandler() override = default;
+
+private:
+	std::shared_ptr<std::queue<std::shared_ptr<ICommand>>> queue;
+};
+
+class RepeatCommandByQueueHandler : public IExeptionHandler {
+public:
+	explicit RepeatCommandByQueueHandler(std::shared_ptr<std::queue<std::shared_ptr<ICommand>>> q);
+
+	[[nodiscard]] bool canHandleException(const std::exception& e) const override;
+
+	void handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const override;
+
+	~RepeatCommandByQueueHandler() override = default;
+
+private:
+	std::shared_ptr<std::queue<std::shared_ptr<ICommand>>> queue;
+};
+
+class RepeatAndWriteToLogHandler : public IExeptionHandler {
+public:
+	explicit RepeatAndWriteToLogHandler();
+
+	[[nodiscard]] bool canHandleException(const std::exception& e) const override;
+
+	void handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const override;
+
+	~RepeatAndWriteToLogHandler() override = default;
+};
+
+class RepeatTwiceAndWriteToLogHandler : public IExeptionHandler {
+public:
+	explicit RepeatTwiceAndWriteToLogHandler();
+
+	[[nodiscard]] bool canHandleException(const std::exception& e) const override;
+
+	void handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const override;
+
+	~RepeatTwiceAndWriteToLogHandler() override = default;
 };
 
 class WriteExceptionToLogCommand : public ICommand {
 public:
-    WriteExceptionToLogCommand(std::exception e) : exception(std::move(e))
-    {}
+	explicit WriteExceptionToLogCommand(std::exception e) : exception(std::move(e))
+	{}
 
-    void execute() override
-    {
-        std::cerr << "exception: " << exception.what() << std::endl;
-    }
+	void execute() override
+	{
+		std::cerr << "exception: " << exception.what() << std::endl;
+	}
 
-    ~WriteExceptionToLogCommand() = default;
+	~WriteExceptionToLogCommand() override = default;
 
 private:
-    std::exception exception;
+	std::exception exception;
 };
 
 class RepeatCommand : public ICommand {
 public:
-    RepeatCommand(std::shared_ptr<ICommand> cmd) : command(std::move(cmd))
-    {}
+	explicit RepeatCommand(std::shared_ptr<ICommand> cmd) : command(std::move(cmd))
+	{}
 
-    void execute() override
-    {
-        command->execute();
-    }
+	void execute() override
+	{
+		command->execute();
+	}
 
-    ~RepeatCommand() = default;
+	~RepeatCommand() override = default;
 
 private:
-    std::shared_ptr<ICommand> command;
+	std::shared_ptr<ICommand> command;
 };
 
+class RepeatTwiceCommand : public ICommand {
+	public:
+	explicit RepeatTwiceCommand(std::shared_ptr<ICommand> cmd) : command(std::make_shared<RepeatCommand>(cmd))
+	{}
 
+	void execute() override
+	{
+		command->execute();
+	}
 
-#endif //PROGLIB_HOMEWORK_EXCEPTION_HANDLER_H
+	~RepeatTwiceCommand() override = default;
+
+private:
+	std::shared_ptr<ICommand> command;
+};
+
+#endif // PROGLIB_HOMEWORK_EXCEPTION_HANDLER_H
--- exceptionhandler/source/exception_handler.cpp
+++ exceptionhandler/source/exception_handler.cpp
@@ -1,8 +1,83 @@
 #include "exception_handler.h"
 
-void ExeptionHandler::registerHandler(std::shared_ptr<IExeptionHandler> handler)
+#include <utility>
+
+void HandlerPool::registerHandler(std::shared_ptr<IExeptionHandler> handler)
 {
-    handlers.push_back(handler);
+	handlers.push_back(handler);
 }
 
-void ExeptionHandler::handleExeption(std::shared_ptr<ICommand> cmd, const std::exception &e) {}
+void HandlerPool::handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const
+{
+	for(const auto& h: handlers) {
+		if(h->canHandleException(e)) {
+			h->handleExeption(cmd, e);
+		}
+	}
+}
+
+WriteExceptionToLogByQueueHandler::WriteExceptionToLogByQueueHandler(
+    std::shared_ptr<std::queue<std::shared_ptr<ICommand>>> q) :
+    queue(std::move(q))
+{}
+
+bool WriteExceptionToLogByQueueHandler::canHandleException(const std::exception& e) const
+{
+	return true;
+}
+
+void WriteExceptionToLogByQueueHandler::handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const
+{
+	queue->push(std::make_shared<WriteExceptionToLogCommand>(e));
+}
+
+RepeatCommandByQueueHandler::RepeatCommandByQueueHandler(std::shared_ptr<std::queue<std::shared_ptr<ICommand>>> q) :
+    queue(std::move(q))
+{}
+
+bool RepeatCommandByQueueHandler::canHandleException(const std::exception& e) const
+{
+	return true;
+}
+
+void RepeatCommandByQueueHandler::handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const
+{
+	queue->push(std::make_shared<RepeatCommand>(cmd));
+}
+
+RepeatAndWriteToLogHandler::RepeatAndWriteToLogHandler()
+{}
+
+bool RepeatAndWriteToLogHandler::canHandleException(const std::exception& e) const
+{
+	return true;
+}
+
+void RepeatAndWriteToLogHandler::handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const
+{
+	try {
+		RepeatCommand repeatCmd(cmd);
+		repeatCmd.execute();
+	}
+	catch(const std::exception& ex) {
+		WriteExceptionToLogCommand logCmd(ex);
+		logCmd.execute();
+	}
+}
+
+RepeatTwiceAndWriteToLogHandler::RepeatTwiceAndWriteToLogHandler()
+{}
+
+bool RepeatTwiceAndWriteToLogHandler::canHandleException(const std::exception& e) const
+{
+	return true;
+}
+
+void RepeatTwiceAndWriteToLogHandler::handleExeption(std::shared_ptr<ICommand> cmd, const std::exception& e) const
+{
+	RepeatTwiceCommand repeatTwiceCmd(cmd);
+	repeatTwiceCmd.execute();
+
+	WriteExceptionToLogCommand logCmd(e);
+	logCmd.execute();
+}
