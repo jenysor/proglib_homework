43b76b025176b77fd80e6a85234506366ddcb31a
e.sorokina <e.sorokina@stc-spb.ru>
2025-09-03 15:59:40 +0300

    move command endable - удаление из очереди

--- movable/source/endable.cpp
+++ movable/source/endable.cpp
@@ -1,5 +1,6 @@
 #include "endable.h"
 #include "adapters.h"
+#include "move_command.h"
 
 EndMoveCommand::EndMoveCommand(std::shared_ptr<IUObject> obj,
                                std::shared_ptr<std::queue<std::shared_ptr<ICommand>>> q) :
@@ -11,5 +12,18 @@ void EndMoveCommand::execute()
 	VelocityAdapter adapter(movingObject);
 	adapter.removeVelocity();
 
-	//удаление из очереди
+	auto size = queue->size();
+	std::queue<std::shared_ptr<ICommand>> newQueue;
+
+	for(size_t num = 0; num < size; ++num) {
+		const auto& command = queue->front();
+
+		if(!dynamic_cast<MoveCommand*>(command.get())) {
+			newQueue.push(command);
+		}
+
+		queue->pop();
+	}
+
+	*queue = newQueue;
 }
