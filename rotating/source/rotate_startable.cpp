d1893a82ac161234cd4e6260786a290e902d0102
e.sorokina <e.sorokina@stc-spb.ru>
2025-09-24 17:12:24 +0300

    start rotate command

--- rotating/source/rotate_startable.cpp
+++ rotating/source/rotate_startable.cpp
@@ -2,4 +2,15 @@
 #include "adapters.h"
 #include "rotate_command.h"
 
+StartRotateCommand::StartRotateCommand(std::shared_ptr<IUObject> obj,
+                                       std::shared_ptr<std::queue<std::shared_ptr<ICommand>>> q, double angVelocity) :
+    movingObject(std::move(obj)), queue(std::move(q)), angVelocity(angVelocity)
+{}
 
+void StartRotateCommand::execute()
+{
+	AngularVelocityAdapter adapter(movingObject);
+	adapter.setAngularVelocity(angVelocity);
+
+	queue->push(std::make_shared<RotateCommand>(movingObject));
+}
